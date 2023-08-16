bb81a61629ba667cd2f087aecb3cf577e2eb6d8d
davidovicd <davidovicd620@gmail.com>
2023-08-16 09:58:24 +0200

    finished a function that adds new book

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
+let newBookBtn = document.querySelector(".newBook");
+let newBookForm = document.querySelector("#newBookForm")
+let add = document.querySelector(".add");
+let library = document.querySelector(".books");
+
 const books = [];
+
+newBookBtn.addEventListener("click", () => newBookForm.classList.remove("hidden"));
+
+add.addEventListener("click", (event)=> {
+  event.preventDefault();
+  addBook();
+  newBookForm.classList.add("hidden");
+})
 
 function Book(image, title, author, description, pages, read ) {
   this.image = image;
@@ -9,6 +22,15 @@ function Book(image, title, author, description, pages, read ) {
   this.read = read;
 }
 
-function addBookLibrary(){
-  // do something here
+function addBook(){
+  let imgLink = document.querySelector("#image-link").value;
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let description = document.querySelector("#description").value;
+  let read = document.querySelector("#read").checked;
+
+  let newBook = new Book(imgLink, title, author, description, pages, read);
+  books.push(newBook);
+  render();
 }

a449e198775bd82a86b027606405c7dd18cc1e98
davidovicd <davidovicd620@gmail.com>
2023-08-16 09:46:04 +0200

    Added book constructor function

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const books = [];
 
-function Book() {
-  // constructor
+function Book(image, title, author, description, pages, read ) {
+  this.image = image;
+  this.title = title;
+  this.author = author;
+  this.description = description;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookLibrary(){
